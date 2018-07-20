3bb287397b16c77439dc28129357f7a4e5f4a227
ryotas <ryotayamanaka@gmail.com>
2018-07-20 15:54:40 +0900

    added node_plasmid

--- create_edge_similarity.sh
+++ create_edge_similarity.sh
@@ -5,9 +5,10 @@ rm data/edge_simirality.pg
 for col in {10..64}; do
 
 plasmid_id=`awk -v FS=',' '{if (NR == 2) print $'$col'}' data/SupplementaryTableS2.csv`
+plasmid_id=${plasmid_id%% (*}
 
 awk -v FS=',' -v OFS='\t' -v "P=$plasmid_id" \
-'{if (NR > 2) { if ($'$col' != 0) print P, $1, "similarity:"$'$col'}}' \
+'{if (NR > 2) { if ($'$col' != 0) print P, $1, ":similar", "score:"$'$col'}}' \
 data/SupplementaryTableS2.csv >> data/edge_simirality.pg 
 
 done
---
+++ create_node_plasmid.sh
@@ -0,0 +1,3 @@
+awk -v FS=',' -v OFS='\t' \
+'{print $1, ":plasmid", "name:"$1, "group:"$2, "class:"$4, "order:"$5, "species:"$6, "genbank:"$7}' \
+data/TableS4.csv > data/node_plasmid.pg
